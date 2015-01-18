c8f78a9acfcdcf84f6b8eee502c8f604a777e083
PilotGtec <erika.fille@gmail.com>
2015-01-18 20:11:28 +0800

    first commit

---
+++ server.R
@@ -0,0 +1,49 @@
+library(shiny)
+library(maps)
+library(googleVis)
+library(datasets)
+
+df <- read.csv("pop2010.csv")
+
+shinyServer( function(input, output) {
+    variable <- reactive({switch(input$choice,
+                                 "Population" = "pop",
+                                 "Population Density" = "popden")})
+    
+    output$gvis <- renderGvis({
+        o <- variable()
+        if(o=="pop") {
+            print("pop")
+            output$header <- renderText({paste("Population")})
+            plotme <- data.frame(df$Code, df$Pop2010)
+            colnames(plotme) <- c("region", "value")
+        }
+        
+        if(o=="popden") {
+            print("popDen")
+            output$header <- renderText({paste("Population Density")})
+            plotme <- data.frame(df$Code, df$PopDen2010)
+            colnames(plotme) <- c("region", "value")
+        }
+        
+        print (plotme)
+        
+        gvisGeoChart(plotme, "region", "value", 
+                     options=list(region="PH", resolution="provinces", title="Philippine Population",
+                                  width=600, height=400))
+        #         names(stateData)[2] <- "value"
+        #         generate the choropleth
+        #         data(stateMapEnv)
+        #         stateData$state.abb <- state.abb[match(USArrests$State, state.name)]
+        #         gvisGeoChart(stateData, "state.abb", "value", options=list(
+        #             region="US", displayMode="regions", resolution="provinces"))
+        #         gvisGeoChart(LifeCycleSavings, "countries", "sr", 
+        #                      options=list(title="Life Cycle Savings",
+        #                                   projection="kavrayskiy-vii"))
+    })
+    
+    newdf <- data.frame(df$Code, df$Name, df$Pop2010, df$PopDen2010)
+    colnames(newdf) <- c( "ISO 3166-2:PH", "Region Name", "Population", "Population Density")
+    output$RegCodes <- renderDataTable(newdf, options = list(pageLength=10))
+    
+} )
---
+++ ui.R
@@ -0,0 +1,22 @@
+library(shiny) 
+
+shinyUI(
+    pageWithSidebar(
+        headerPanel("2010 Philippine Population"),
+        sidebarPanel(
+            p("The Philippines is composed of 7,107 islands and 
+              is divided into 17 administrative regions. Here, we look at how 
+              the popluation is distributed across the islands."),
+            selectInput("choice", "Select a Population Measure:",
+                        choices = c("Population", "Population Density"))),
+        mainPanel(h3(textOutput("header")),
+                  #p(uiOutput("avg")),
+                  #p(uiOutput("sd")),
+                  #p("Hover over a state to see the number of arrests. The horizontal bar below the chart shows the max & min values."),
+                  htmlOutput("gvis"),
+                  h3("Data Table"),
+                  p("Here we list the equivalent code needed to plot the values on the map."),
+                  dataTableOutput("RegCodes")
+        )       
+    ))
+
